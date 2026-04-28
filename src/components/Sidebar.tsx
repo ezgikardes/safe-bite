01cd6d04a64147e19dc2bad4029ec76c48573ca3
ezgikardes <ezgikardes@gmail.com>
2026-04-28 09:52:23 +0200

    feat: update Sidebar component to include navigation links

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,3 +1,13 @@
+import { NavLink } from "react-router-dom";
+
 export default function Sidebar() {
-  return <aside>Sidebar</aside>;
+  return (
+    <aside>
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/search">Search</NavLink>
+      <NavLink to="/my-safe-foods">My Safe Foods</NavLink>
+      <NavLink to="/my-triggers">My Triggers</NavLink>
+      <NavLink to="/about">About</NavLink>
+    </aside>
+  );
 }
