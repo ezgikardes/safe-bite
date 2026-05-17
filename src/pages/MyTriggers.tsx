c02474089982cd9c0711c2ad445546452157df94
ezgikardes <ezgikardes@gmail.com>
2026-05-17 14:06:32 +0200

    feat: implement trigger management with local storage integration in MyTriggers

--- src/pages/MyTriggers.tsx
+++ src/pages/MyTriggers.tsx
@@ -1,3 +1,29 @@
+import { useEffect, useState } from "react";
+
 export default function MyTriggers() {
-  return <div>MyTriggers</div>;
+  const [savedTriggers, setSavedTriggers] = useState<string[]>(() => {
+    try {
+      return JSON.parse(localStorage.getItem("myTriggers") ?? "[]");
+    } catch {
+      return [];
+    }
+  });
+
+  const [newTrigger, setNewTrigger] = useState<string>("");
+
+  useEffect(() => {
+    localStorage.setItem("myTriggers", JSON.stringify(savedTriggers));
+  }, [savedTriggers]);
+
+  function addTrigger() {
+    setSavedTriggers([...savedTriggers, newTrigger]);
+    setNewTrigger("");
+  }
+
+  function removeTrigger(trigger: string) {
+    setSavedTriggers(savedTriggers.filter((t) => t !== trigger));
+  }
+
+  return;
+  <></>;
 }
