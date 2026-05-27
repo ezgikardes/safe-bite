b6b1e187fcf92e5a76237ebade4f48a6522834f2
ezgikardes <ezgikardes@gmail.com>
2026-05-27 23:20:11 +0200

    feat: add functionality to save products to favorites in ProductCard component

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -13,9 +13,28 @@ export default function ProductCard({ product }: ProductCardProps) {
     risky: "🔴 Risky",
   };
 
+  const savedProducts: object[] = JSON.parse(
+    localStorage.getItem("myFavorites") ?? "[]",
+  );
+
+  function addToFavorites(newProduct: Product) {
+    localStorage.setItem(
+      "myFavorites",
+      JSON.stringify([...savedProducts, newProduct]),
+    );
+  }
+
   return (
     <Link to={`/product/${product.code}`}>
       <img src={product.image_front_url} />
+      <button
+        onClick={(e) => {
+          e.preventDefault();
+          e.stopPropagation();
+          addToFavorites(product);
+        }}>
+        add to favorites
+      </button>
       <p>{product.product_name}</p>
       <p>{product.brands}</p>
       <p>
